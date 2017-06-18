6c35ea5e398f5a0205dfe0e8a95ef1ae0a3a9437
AlexKing1028 <940042441@qq.com>
2017-06-18 23:42:28 +0800

    fix a bug

--- src/main/equipment/EquipmentModel.java
+++ src/main/equipment/EquipmentModel.java
@@ -73,7 +73,7 @@ public class EquipmentModel {
                     return "invalid id";
                 case Constant.OPEN_REQUEST:
                     if (AuthModel.isCommander() && collected_keys!=null){
-                        Key k = Key.fromBytes(data, 1);
+                        Key k = Key.fromBytes(data, 2);
                         HashSet<Key> sk = addKey(e, k);
                         int result =  e.tryUnlock(sk);
                         if (result == 1){
